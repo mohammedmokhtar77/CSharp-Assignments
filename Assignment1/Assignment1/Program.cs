8aa6c8e3eb8b901dd83101d3cdc6ddc210a61a03
Mohammed Mokhtar <mohammed.mokhtar.softwareeng@gmail.com>
2026-08-30 15:56:31 +0300

    feat: solve question 1 - read and print a number

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Q1 : Write a program that allows the user to enter a number then print it. 
+            #region Question 1
+            Console.WriteLine("Enter Number:");
+            string? input = Console.ReadLine();
+            if (int.TryParse(input, out int number))
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("Enter Invalid Number");
+            }
+            #endregion
+        }
+    }
+}

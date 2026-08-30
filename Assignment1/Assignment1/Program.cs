9934db284af1fee72b4cc44178b4f859884ba6f4
Mohammed Mokhtar <mohammed.mokhtar.softwareeng@gmail.com>
2026-08-30 16:14:48 +0300

    feat: solve question 3 - perform floating-point arithmetic

--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -31,6 +31,37 @@
                 Console.WriteLine("FormatException: The input contains non-numeric characters.");
             }
             #endregion
+
+            /*
+                Q3 : Write C# program that Perform a simple arithmetic 
+                   operation with floating-point numbers And mention 
+                   what will happen ? 
+                       Answer: Floating-point numbers may not represent all decimal values with exact precision, 
+                       so arithmetic operations can sometimes produce a value that is slightly different from the expected result 
+                       due to floating-point precision limitations.
+            */
+            #region Question 3
+
+            Console.Write("Enter first floating-point number: ");
+            string? input1 = Console.ReadLine();
+            Console.Write("Enter second floating-point number: ");
+            string? input2 = Console.ReadLine();
+
+            if (float.TryParse(input1, out float num1) && float.TryParse(input2, out float num2))
+            {
+                float sum = num1 + num2;
+                float sub = num1 - num2;
+                float multi = num1 * num2;
+
+                Console.WriteLine($"Sum: {sum}");
+                Console.WriteLine($"Subtract: {sub}");
+                Console.WriteLine($"Multiply: {multi}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+            #endregion
         }
     }
 }

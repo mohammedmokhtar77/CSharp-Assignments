321edf7e296dee60ca63b5630c89847e3f0768ca
Mohammed Mokhtar <mohammed.mokhtar.softwareeng@gmail.com>
2026-09-01 00:46:54 +0300

    feat: solve question 1 - check divisibility by 3 and 4

--- Assignment2/Assignment2/Program.cs
+++ Assignment2/Assignment2/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace Assignment2;
+﻿using System.Globalization;
+
+namespace Assignment2;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        #region Question1
+        
+        /* 1- Write a program that takes a number from the user then print yes if 
+        that number can be divided by 3 and 4 otherwise print no. */
+        
+        string? input = Console.ReadLine();
+
+        if (int.TryParse(input, out int number))
+        {
+            if (number % 3 == 0 && number % 4 == 0)
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input.");
+        }
+        #endregion
     }
 }

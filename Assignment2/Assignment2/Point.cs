d99b43ebb50cec9528a8c957b61c3295cf2e679d
Mohammed Mokhtar <mohammed.mokhtar.softwareeng@gmail.com>
2026-09-01 16:18:27 +0300

    Add Default Constructor.

--- Assignment2/Assignment2/Point.cs
+++ Assignment2/Assignment2/Point.cs
@@ -5,6 +5,10 @@ public class Point
     public double X { get; set; }
     public double Y { get; set; }
 
+    public Point()
+    {
+        
+    }
     public Point(double x,double y)
     {
         X = x;

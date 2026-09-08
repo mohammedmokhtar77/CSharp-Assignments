5c4c281ba785bfb965faf3cf0d4a1ada16c9bbe2
Mohammed Mokhtar <mohammed.mokhtar.softwareeng@gmail.com>
2026-09-09 00:36:31 +0300

    feat: solve question 1 - explain pass by value and pass by reference (value type parameter).

--- Assignment3/Assignment3/Program.cs
+++ Assignment3/Assignment3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        #region Question1
+
+        /*
+        1- Explain the difference between passing value type parameters
+        by value and by reference then write a suitable C# example.
+        */
+
+        // Pass by Value
+        static void PassByValue(int number)
+        {
+            number = 100;
+        }
+
+        // Pass by Reference
+        static void PassByReference(ref int number)
+        {
+            number = 100;
+        }
+
+        int x = 10;
+        int y = 10;
+
+        PassByValue(x);
+        PassByReference(ref y);
+
+        Console.WriteLine($"After Pass By Value: {x}"); // Still 10 
+        Console.WriteLine($"After Pass By Reference: {y}"); // Changed to 100
+
+        #endregion
+        
+        
     }
 }

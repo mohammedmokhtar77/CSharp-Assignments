bfb645b3ed7eff405a12311691a8667c0a43f21d
Mohammed Mokhtar <mohammed.mokhtar.softwareeng@gmail.com>
2026-09-09 01:48:14 +0300

    Add Point Struct

---
+++ Assignment3/Assignment3/Point.cs
@@ -0,0 +1,13 @@
+namespace Assignment3;
+
+public struct Point
+{
+    public double X { get; }
+    public double Y { get; }
+
+    public Point(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+}

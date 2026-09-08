e080e559f65c7b0164c263177b7c253417902c32
Mohammed Mokhtar <mohammed.mokhtar.softwareeng@gmail.com>
2026-09-09 01:29:23 +0300

    Add Struct Person

---
+++ Assignment3/Assignment3/SPerson.cs
@@ -0,0 +1,14 @@
+namespace Assignment3;
+
+public struct SPerson
+{
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public SPerson(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+}

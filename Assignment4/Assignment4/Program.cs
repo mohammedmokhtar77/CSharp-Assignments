8861fb621e1716edaa677675d6dc6ecd20892910
Mohammed Mokhtar <mohammed.mokhtar.softwareeng@gmail.com>
2026-09-13 13:56:53 +0300

    feat: initialize Employee array and display employee details

--- Assignment4/Assignment4/Program.cs
+++ Assignment4/Assignment4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Employee[] Employees = new Employee[3];
+        Employees[0] = new Employee(
+            1,
+            "Mokhtar",
+            SecurityLevel.DBA,
+            30000m,
+            new HiringDate(18, 4, 1981),
+            Gender.Male
+        );
+
+        Employees[1] = new Employee(
+            2,
+            "Ali",
+            SecurityLevel.Guest,
+            12000m,
+            new HiringDate(29, 7, 2003),
+            Gender.Female
+        );
+
+        Employees[2] = new Employee(
+            3,
+            "Mohammed",
+            SecurityLevel.SecurityOfficer,
+            40000m,
+            new HiringDate(10, 1, 2006),
+            Gender.Male
+        );
+
+        foreach (Employee employee in Employees)
+        {
+            Console.WriteLine(employee);
+        }
+
     }
 }

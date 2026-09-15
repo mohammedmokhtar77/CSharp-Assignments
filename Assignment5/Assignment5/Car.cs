cf42d383b7fd32d6d9ceb0a66084791ddf1ae3f5
Mohammed Mokhtar <mohammed.mokhtar.softwareeng@gmail.com>
2026-09-15 18:05:36 +0300

    feat: add Car class with methods for movement in multiple directions

---
+++ Assignment5/Assignment5/Car.cs
@@ -0,0 +1,24 @@
+namespace Assignment5;
+
+public class Car
+{
+    public void MoveForward()
+    {
+        Console.WriteLine("Car is moving forward.");
+    }
+
+    public void MoveBackward()
+    {
+        Console.WriteLine("Car is moving backward.");
+    }
+
+    public void MoveUp()
+    {
+        Console.WriteLine("Car is moving up.");
+    }
+
+    public void MoveDown()
+    {
+        Console.WriteLine("Car is moving down.");
+    }
+}

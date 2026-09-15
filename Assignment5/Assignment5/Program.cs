99870931d87c93713d71d74c1176d0638c1081a9
Mohammed Mokhtar <mohammed.mokhtar.softwareeng@gmail.com>
2026-09-15 18:01:13 +0300

    feat: organize Main method with regions and add ProcessPerson function for Doctor and Engineer instances

--- Assignment5/Assignment5/Program.cs
+++ Assignment5/Assignment5/Program.cs
@@ -4,7 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        #region Q3
+        #region Part 1: Binding
+
+        #region Q1, Q2, Q3
 
         Shape shape = new Shape(2, 3);
         Console.WriteLine(shape.Area()); // 6
@@ -30,6 +32,46 @@ class Program
          */
 
         #endregion
+
+        #region Q5, Q6, Q7, Q8
+
+        static void ProcessPerson(Person person)
+        {
+            person.Greet();
+            person.Display();
+        }
+        
+        Person doctor = new Doctor { Id = 1, Name = "Ali", Age = 35, 
+            Specialty = "Cardiology" };
+
+        Person engineer = new Engineer { Id = 2, Name = "Mohammed", Age = 20,
+            Field = "Software Engineering", YearsOfExperience = 5 };
+
+        ProcessPerson(doctor);
+        Console.WriteLine("-------------------------------------------");
+        ProcessPerson(engineer);
+        
+        /*
+        Static Binding:
+              Greet() is non-virtual, so the method is resolved at compile time
+              based on the reference type (Person), therefore Person.Greet() is called.
+        Dynamic Binding:
+              Display() is virtual, so the method is resolved at runtime
+              based on the actual object type (Doctor or Engineer), therefore
+              the overridden derived Display() method is called.
+              
+              If I removed Virtual From Base Without Remove Override from childes:
+                  CS0506: 'Doctor.Display()': cannot override inherited member 
+                  'Person.Display()' because it is not marked virtual, abstract, or override
+              If I removed Virtual From Base With Remove Override from childes:
+                  Static Binding (Line 51)
+         */
+        
+
+        #endregion  
+
+        #endregion
+        
         
         
         

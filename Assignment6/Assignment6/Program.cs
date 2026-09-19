e6ee4b2110b5100a8b9b3375db0cdf2c01d98264
Mohammed Mokhtar <mohammed.mokhtar.softwareeng@gmail.com>
2026-09-19 23:43:21 +0300

    feat: add operator testing examples for Duration class in Program.cs

--- Assignment6/Assignment6/Program.cs
+++ Assignment6/Assignment6/Program.cs
@@ -139,8 +139,69 @@ class Program
         // Console.WriteLine(d4.ToString()); // Minutes :11, Seconds :6
 
         #endregion
+
+        #region Testing Operators
+
+        Duration D1 = new Duration(10, 40, 30);
+        Duration D2 = new Duration(20, 50, 60);
+
+        Console.WriteLine("D1 = " + D1);
+        Console.WriteLine("D2 = " + D2);
         
+        Duration D3 = D1 + D2;
+        Console.WriteLine($"D3 = {D3}");
         
+        D3 = D1 + 7800;
+        Console.WriteLine($"D3 = {D3}");
+
+
+
+        D3 = 666 + D3;
+        Console.WriteLine($"D3 = {D3}");
+        
+        D3 = ++D1;
+
+        Console.WriteLine($"D1 = {D1}");
+        Console.WriteLine($"D3 = {D3}");
+        
+        D3 = --D2;
+
+        Console.WriteLine($"D2 = {D2}");
+        Console.WriteLine($"D3 = {D3}");
+        
+        D1 = D1 - D2;
+        Console.WriteLine($"D1 = {D1}");
+
+        
+        if (D1 > D2)
+        {
+            Console.WriteLine("D1 is greater than D2");
+        }
+        else
+        {
+            Console.WriteLine("D1 is NOT greater than D2");
+        }
+
+        
+        if (D1 <= D2)
+        {
+            Console.WriteLine("D1 is less than or equal to D2");
+        }
+        else
+        {
+            Console.WriteLine("D1 is NOT less than or equal to D2");
+        }
+        
+        if (D1)
+        {
+            Console.WriteLine("D1 has a duration greater than zero.");
+        }
+        else
+        {
+            Console.WriteLine("D1 is zero.");
+        }
+
+        #endregion
         
     }
 }

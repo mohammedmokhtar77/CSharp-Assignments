5f691690f2a32b69b450c1cc616f7c0c261ffaf5
Mohammed Mokhtar <mohammed.mokhtar.softwareeng@gmail.com>
2026-09-19 23:24:15 +0300

    feat: add Math class with basic arithmetic operations

---
+++ Assignment6/Assignment6/Math.cs
@@ -0,0 +1,30 @@
+namespace Assignment6;
+
+public class Math
+{
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
+
+    public static int Subtract(int x, int y)
+    {
+        return x - y;
+    }
+
+    public static int Multiply(int x, int y)
+    {
+        return x * y;
+    }
+
+    public static double Divide(int x, int y)
+    {
+        if (y == 0)
+        {
+            Console.WriteLine("Cannot divide by zero.");
+            return 0;
+        }
+
+        return (double)x / y;
+    }
+}

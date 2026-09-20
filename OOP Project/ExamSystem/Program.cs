f00b5a25d6e854217e0b746959aeed38b16bae7f
Mohammed Mokhtar <mohammed.mokhtar.softwareeng@gmail.com>
2026-09-20 21:44:27 +0300

    feat: implement exam creation and display in Main method

--- OOP Project/ExamSystem/Program.cs
+++ OOP Project/ExamSystem/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Subject subject = new Subject(1, "C#");
+
+        Answer answer1 = new Answer(1, "C#");
+        Answer answer2 = new Answer(2, "Java");
+        Answer answer3 = new Answer(3, "Python");
+        Answer answer4 = new Answer(4, "C++");
+
+        Answer[] answers = { answer1, answer2, answer3, answer4 };
+
+        Question question = new MCQQuestion("Q1", 
+            "Which language is used to build .NET applications?", 
+            60, answers, answer1);
+
+        Question[] questions = { question };
+        // Dynamic Binding (Late Binding) ===> CLR At Runtime
+        Exam exam = new FinalExam(60, questions.Length, questions, subject);
+        subject.CreateExam(exam);
+        exam.ShowExam();
     }
 }

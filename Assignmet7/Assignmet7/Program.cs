5ea7675de4cc4a4e7fa2837c220f7c4ec4cb452c
Mohammed Mokhtar <mohammed.mokhtar.softwareeng@gmail.com>
2026-09-21 22:50:23 +0300

    feat: add demonstration of PatientDto functionality in Program.cs

--- Assignmet7/Assignmet7/Program.cs
+++ Assignmet7/Assignmet7/Program.cs
@@ -6,6 +6,8 @@ class Program
     {
         #region Primary Constructor & Records
 
+        #region Primary Constructor
+
         Patient patient1 = new Patient(1, "Mohammed Mokhtar", "01111111111", "Good");
         
         Patient patient2 = new Patient(1, "Mohammed Mokhtar", "01111111111", "Good");
@@ -21,6 +23,30 @@ class Program
         Console.WriteLine($"Patient1 Hash Code : {patient1.GetHashCode()} " +
                           $":: Patient Hash Code : {patient2.GetHashCode()} " +
                           $":: Patient1 = Patient ? : {patient2.Equals(patient1)} ");
+
+        #endregion
+
+        #region Records
+
+        PatientDto patientDto1 = new PatientDto(1, "Mohammed Mokhtar", "01111111111");
+        
+        PatientDto patientDto2 = new PatientDto(1, "Mohammed Mokhtar", "01111111111");
+
+        Console.WriteLine("Before");
+        Console.WriteLine($"Patient1 Hash Code : {patientDto1.GetHashCode()} " +
+                          $":: Patient Hash Code : {patientDto2.GetHashCode()} " +
+                          $":: Patient1 = Patient ? : {patientDto2.Equals(patientDto1)} ");
+        
+        patientDto2 = patientDto1;
+        
+        Console.WriteLine("After");
+        Console.WriteLine($"Patient1 Hash Code : {patientDto1.GetHashCode()} " +
+                          $":: Patient Hash Code : {patientDto2.GetHashCode()} " +
+                          $":: Patient1 = Patient ? : {patientDto2.Equals(patientDto1)} ");
+
+        #endregion
+        
+        
 
         #endregion
     }

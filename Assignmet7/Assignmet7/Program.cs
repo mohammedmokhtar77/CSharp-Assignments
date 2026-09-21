8cba834e375441659dc648180399a5c56970f964
Mohammed Mokhtar <mohammed.mokhtar.softwareeng@gmail.com>
2026-09-21 23:01:01 +0300

    chore: comment out demonstration code for Patient class behavior in Program.cs

--- Assignmet7/Assignmet7/Program.cs
+++ Assignmet7/Assignmet7/Program.cs
@@ -7,22 +7,22 @@ class Program
         #region Primary Constructor & Records
 
         #region Primary Constructor
-
-        Patient patient1 = new Patient(1, "Mohammed Mokhtar", "01111111111", "Good");
-        
-        Patient patient2 = new Patient(1, "Mohammed Mokhtar", "01111111111", "Good");
-
-        Console.WriteLine("Before");
-        Console.WriteLine($"Patient1 Hash Code : {patient1.GetHashCode()} " +
-                          $":: Patient Hash Code : {patient2.GetHashCode()} " +
-                          $":: Patient1 = Patient ? : {patient2.Equals(patient1)} ");
-        
-        patient2 = patient1;
-        
-        Console.WriteLine("After");
-        Console.WriteLine($"Patient1 Hash Code : {patient1.GetHashCode()} " +
-                          $":: Patient Hash Code : {patient2.GetHashCode()} " +
-                          $":: Patient1 = Patient ? : {patient2.Equals(patient1)} ");
+        //
+        // Patient patient1 = new Patient(1, "Mohammed Mokhtar", "01111111111", "Good");
+        //
+        // Patient patient2 = new Patient(1, "Mohammed Mokhtar", "01111111111", "Good");
+        //
+        // Console.WriteLine("Before");
+        // Console.WriteLine($"Patient1 Hash Code : {patient1.GetHashCode()} " +
+        //                   $":: Patient Hash Code : {patient2.GetHashCode()} " +
+        //                   $":: Patient1 = Patient ? : {patient2.Equals(patient1)} ");
+        //
+        // patient2 = patient1;
+        //
+        // Console.WriteLine("After");
+        // Console.WriteLine($"Patient1 Hash Code : {patient1.GetHashCode()} " +
+        //                   $":: Patient Hash Code : {patient2.GetHashCode()} " +
+        //                   $":: Patient1 = Patient ? : {patient2.Equals(patient1)} ");
 
         #endregion
 

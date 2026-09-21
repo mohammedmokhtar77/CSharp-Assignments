cfbbb46d1cc17a86fcaaee7455bf9da4c080693a
Mohammed Mokhtar <mohammed.mokhtar.softwareeng@gmail.com>
2026-09-21 23:00:39 +0300

    feat: refactor Patient class to use properties for Id, FullName, PhoneNumber, and MedicalHistory

--- Assignmet7/Assignmet7/Patient.cs
+++ Assignmet7/Assignmet7/Patient.cs
@@ -1,7 +1,13 @@
 namespace Assignmet7;
 
-public class Patient(int Id , string FullName , string PhoneNumber , string MedicalHistory)
+public class Patient()
 {
+    public int Id { get; set; }
+    public string? FullName { get; set; }
+    
+    public string? PhoneNumber { get; set; }
+    
+    public string? MedicalHistory { get; set; }
     public override string ToString()
     {
         return $"ID : {Id} :: Full Name : {FullName} " +

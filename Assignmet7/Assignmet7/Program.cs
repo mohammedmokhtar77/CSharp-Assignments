4c9daa8f423e63717b057fae7d3faa2ae5909d56
Mohammed Mokhtar <mohammed.mokhtar.softwareeng@gmail.com>
2026-09-21 22:44:59 +0300

    feat: add demonstration of Patient class functionality in Main method

--- Assignmet7/Assignmet7/Program.cs
+++ Assignmet7/Assignmet7/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main()
     {
+        #region Primary Constructor & Records
+
+        Patient patient1 = new Patient(1, "Mohammed Mokhtar", "01111111111", "Good");
         
+        Patient patient2 = new Patient(1, "Mohammed Mokhtar", "01111111111", "Good");
+
+        Console.WriteLine("Before");
+        Console.WriteLine($"Patient1 Hash Code : {patient1.GetHashCode()} " +
+                          $":: Patient Hash Code : {patient2.GetHashCode()} " +
+                          $":: Patient1 = Patient ? : {patient2.Equals(patient1)} ");
+        
+        patient2 = patient1;
+        
+        Console.WriteLine("After");
+        Console.WriteLine($"Patient1 Hash Code : {patient1.GetHashCode()} " +
+                          $":: Patient Hash Code : {patient2.GetHashCode()} " +
+                          $":: Patient1 = Patient ? : {patient2.Equals(patient1)} ");
+
+        #endregion
     }
 }

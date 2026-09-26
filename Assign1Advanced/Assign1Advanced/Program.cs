ac221263a537bfc654311ec60e437a4b3fcbf889
Mohammed Mokhtar <mohammed.mokhtar.softwareeng@gmail.com>
2026-09-26 17:06:40 +0300

    feat: implement optimized bubble sort algorithm in Program class

--- Assign1Advanced/Assign1Advanced/Program.cs
+++ Assign1Advanced/Assign1Advanced/Program.cs
@@ -1,8 +1,39 @@
-﻿namespace Assign1Advanced;
+﻿using System.Runtime.CompilerServices;
+
+namespace Assign1Advanced;
 
 class Program
 {
     static void Main(string[] args)
     {
+
+        #region Optimized Bubble Sort
+
+        int[] numbers = { 1, 2, 3, 5, 4 };
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            bool swapped = false;
+            for (int j = 0; j < numbers.Length - 1 - i; j++)
+            {
+                if (numbers[j] > numbers[j + 1])
+                {
+                    (numbers[j], numbers[j + 1]) = (numbers[j + 1], numbers[j]);
+                    swapped = true;
+                }
+            }
+            if (!swapped)
+                break;
+        }
+        
+        Console.WriteLine("Sorted Array:");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+            
+
+        }
+        #endregion
+        
     }
+    
 }

a590d613b8320ad70e18ce938ec24c5d3b503c56
Mohammed Mokhtar <mohammed.mokhtar.softwareeng@gmail.com>
2026-09-26 17:16:45 +0300

    refactor: comment out example code in Program class for clarity

--- Assign1Advanced/Assign1Advanced/Program.cs
+++ Assign1Advanced/Assign1Advanced/Program.cs
@@ -9,39 +9,41 @@ class Program
 
         #region Optimized Bubble Sort
 
-        int[] numbers = { 1, 2, 3, 5, 4 };
-        for (int i = 0; i < numbers.Length; i++)
-        {
-            bool swapped = false;
-            for (int j = 0; j < numbers.Length - 1 - i; j++)
-            {
-                if (numbers[j] > numbers[j + 1])
-                {
-                    (numbers[j], numbers[j + 1]) = (numbers[j + 1], numbers[j]);
-                    swapped = true;
-                }
-            }
-            if (!swapped)
-                break;
-        }
-        
-        Console.WriteLine("Sorted Array:");
-        foreach (int number in numbers)
-        {
-            Console.Write(number + " ");
-            
-
-        }
+        // int[] numbers = { 1, 2, 3, 5, 4 };
+        // for (int i = 0; i < numbers.Length; i++)
+        // {
+        //     bool swapped = false;
+        //     for (int j = 0; j < numbers.Length - 1 - i; j++)
+        //     {
+        //         if (numbers[j] > numbers[j + 1])
+        //         {
+        //             (numbers[j], numbers[j + 1]) = (numbers[j + 1], numbers[j]);
+        //             swapped = true;
+        //         }
+        //     }
+        //     if (!swapped)
+        //         break;
+        // }
+        //
+        // Console.WriteLine("Sorted Array:");
+        // foreach (int number in numbers)
+        // {
+        //     Console.Write(number + " ");
+        //     
+        //
+        // }
         #endregion
 
         #region Generic Range
 
-        Range<int> range = new Range<int>(10, 20);
-        Console.WriteLine(range.IsInRange(15));
-        Console.WriteLine(range.IsInRange(30));
-        Console.WriteLine(range.Length());
+        // Range<int> range = new Range<int>(10, 20);
+        // Console.WriteLine(range.IsInRange(15));
+        // Console.WriteLine(range.IsInRange(30));
+        // Console.WriteLine(range.Length());
 
         #endregion
+        
+        
     }
     
 }
